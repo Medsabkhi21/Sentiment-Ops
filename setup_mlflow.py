073a5a9718632044c9d6ca84f8c557924bb6d9bf
sabkhi <58121416+Medsabkhi21@users.noreply.github.com>
2023-02-06 19:50:34 +0100

    Delete setup_mlflow.py

--- setup_mlflow.py
+++
@@ -1,4 +0,0 @@
-" My constants"
-MLFLOW_TRACKING_URI= "https://dagshub.com/Medsabkhi21/Sentiment-Ops.mlflow" 
-MLFLOW_TRACKING_USERNAME="Medsabkhi21" 
-MLFLOW_TRACKING_PASSWORD="d001fdad5db868f4e019f59823f7658cb1b25eac"
